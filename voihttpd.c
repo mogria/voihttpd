96339959457496f2ac323ce77dda7956d44429af
Mogria <m0gr14@gmail.com>
2011-12-04 17:07:24 +0100

    the webserver handles now more than one request

--- voihttpd.c
+++ voihttpd.c
@@ -12,6 +12,29 @@
 #define BUFLEN 1024
 #define NAME "voihttpd"
 
+void handleHTTPRequest(int client_socket);
+void error(const char *msg, int nr);
+
+void handleHTTPRequest(int client_socket) {
+  char buffer[BUFLEN];
+
+  bzero(buffer, 256);
+
+  int n;
+
+  n = read(client_socket, buffer, BUFLEN - 1);
+  if(n < 0) {
+    error("can't read from socket", 6);
+  }
+
+  printf("%s\n", buffer);
+
+  n = write(client_socket, "Gotcha!", 7);
+  if(n < 0) {
+    error("can't write to socket", 7);
+  }
+}
+
 void error(const char *msg, int nr) {
   char errorbuffer[256];
   char formatnr[] = "%s: %i: %s";
@@ -31,15 +54,13 @@ int main(int argc, char *argv[]) {
   int sockfd; //socket file descriptor
   int port; //port number
   int clientsocketfd; //socket file descriptor of the client
+  int pid;
+  void (*handle)(int) = handleHTTPRequest;
 
   socklen_t clilen;
 
-  char buffer[BUFLEN];
-
   struct sockaddr_in server_addr; //Server Adress
   struct sockaddr_in cli_addr; //Server Adress
-
-  int n;
 
   // check if enougth arguments are given
   if(argc < 2) {
@@ -68,25 +89,25 @@ int main(int argc, char *argv[]) {
 
   listen(sockfd,5);
   clilen = sizeof(cli_addr);
-  clientsocketfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-  if(clientsocketfd < 0) {
-    error("can't accept", 4);
+  while(1) {
+    clientsocketfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if(clientsocketfd < 0) {
+      error("can't accept", 4);
+    }
+
+    pid = fork();
+    if(pid < 0) {
+      error("can't fork", 5);
+    }
+    
+    if(pid == 0) { //child
+      close(sockfd);
+      handle(clientsocketfd);
+      exit(0);
+    } else { //parent
+      close(clientsocketfd);
+    }
+
   }
-
-  bzero(buffer, 256);
-
-  n = read(clientsocketfd, buffer, BUFLEN - 1);
-  if(n < 0) {
-    error("can't read from socket", 5);
-  }
-
-  printf("%s\n", buffer);
-
-  n = write(clientsocketfd, "Gotcha!", 7);
-  if(n < 0) {
-    error("can't write to socket", 6);
-  }
-
-  close(clientsocketfd);
-  close(sockfd);
+  return 0;
 }
