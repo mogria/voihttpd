e0c73c334180b161b995d6bd7fd2dc57cd312f22
Mogria <m0gr14@gmail.com>
2012-01-30 18:13:32 +0100

    tested some further things

--- test/assoc_array_test.c
+++ test/assoc_array_test.c
@@ -7,8 +7,18 @@ int main ( void ) {
   struct assoc_array array;
   int value = 5;
   assoc_array ( &array );
+  int value2 = 15;
   assoc_set ( &array, "voidness", (void*)&value );
-  printf ( "and the number is .... : %i expected : %i \n", *((int*)assoc_get ( &array, "voidness" )), value);
+  assoc_set ( &array, "voidness", (void*)&value2 );
+  assoc_set ( &array, "voidness2", (void*)&value2 );
+  assoc_set ( &array, "what the fuck", (void*)&value );
+  printf ( "and the number is .... : %i expected : %i \n", *((int*)assoc_get ( &array, "voidness" )), value2);
+  printf ( "and the number is .... : %i expected : %i \n", *((int*)assoc_get ( &array, "voidness2" )), value2);
+  printf ( "and the number is .... : %i expected : %i \n", *((int*)assoc_get ( &array, "what the fuck" )), value);
+  printf ( "position : %i expected %i \n", array.position, 0);
+  assoc_prev ( &array );
+  printf ( "position : %i expected %i \n", array.position, -1);
+  printf ( "res: %p expected %p \n", assoc_current(&array), NULL);
   assoc_print ( &array );
   return 0;
 }
