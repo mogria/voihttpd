864d043e84a44d47b56576bd1d7bc6d1485e400e
Mogria <m0gr14@gmail.com>
2012-02-01 14:30:57 +0100

    wrote test for config.h

---
+++ test/config_test.c
@@ -0,0 +1,22 @@
+
+/**
+ * @author Mogria
+ * @version 0.1
+ * @source TODO #####.c
+ * @description TODO #####
+ * @license GNU/GPL
+ */
+
+
+#include "../lib/config.h"
+
+int main () {
+
+
+  char filename[] = "config_test.cfg";
+
+  read_config(filename);
+
+
+  return 0;
+} 
